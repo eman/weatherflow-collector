252a70d510667188b5d3c16763603f6d82630440
Emmanuel Levijarvi <emansl@gmail.com>
2025-06-04 07:19:20 -0700

    sex x bit

--- weatherflow-collector/build-arm64.sh
+++ weatherflow-collector/build-arm64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build  -f Dockerfile.arm -t eman/weatherflow-collector:latest-arm64 .
+docker build  -f Dockerfile.arm -t eman/weatherflow-collector:latest .
 # docker push lux4rd0/weatherflow-collector:latest-arm64
 # docker push lux4rd0/weatherflow-collector:$1-arm64
